7a37039d5e2b8294a2a5a385f8ce44b7591452be
alber <aicarq@hotmail.com>
2024-05-30 15:29:58 -0600

    sim applyed, responses added for info about game flood

--- app.js
+++ app.js
@@ -2,38 +2,65 @@ const gameBoardObject = (() => {
     let gameBoard = [];
     let count = 0;
     let gameInProcess = false
+    let winner;
 
     const gameStatus = (() => {
         const gameStarted = () => {
             gameInProcess = true;
+            let virtualGame = Math.random();
+            if (virtualGame < .33) {
+                winner = "John Doe";
+            } else if (virtualGame >= .33 && virtualGame <= .66) {
+                winner = "Jane Danna"
+            } else {
+                winner = "tie";
+
+            }
         }
         const gameEnded = () => {
             if (!!gameInProcess) {
                 gameInProcess = false;
-            count++;
-            //CASE PLAYER 1, PLAYER TWO, TIE, ADD TO SCORE
-            switch (winner){
-                case ("John Doe"):
-                    gameBoard[0].score++;
-                    break;
-                case ("Jane Danna"):
-                    gameBoard[1].score++;
-                    break;
-                case ("tie"):
-                    "its a tie";
-                    break
-                }
+                count++;
+                //CASE PLAYER 1, PLAYER TWO, TIE, ADD TO SCORE
+                switch (winner){
+                    case ("John Doe"):
+                        gameBoard[0].score++;
+                        return `${gameBoard[0].playerName}`
+                        break;
+                    case ("Jane Danna"):
+                        gameBoard[1].score++;
+                        return `${gameBoard[1].playerName}`
+                        break;
+                    case ("tie"):
+                        return "its a tie";
+                        break
+                    }
 
             }
+
             
             //CLICK OR TAP TO CLEAN BOARD FOR NEW GAME
         }
-        const restartGame = () => count = 0;
-        return {gameStarted, gameEnded, restartGame}
+
+    const simCompleteGame = ()=> {
+        gameStatus.gameStarted();
+        gameStatus.gameEnded();
+        return `${winner}`
+        // gameBoardObject.gameBoardStats();
+        // gameBoardObject.gamesCounter;        
+    }
+        const restartGame = () => {
+            count = 0;
+            gameBoard[0].score = 0;
+            gameBoard[1].score = 0;
+            gameStarted()
+
+        }
+        return {gameStarted, gameEnded, restartGame, simCompleteGame}
     })()
 
     const gameBoardStats = () => {
-        return gameBoard;
+        return `${gameBoard[0].playerName} : ${gameBoard[0].score}  ${gameBoard[1].playerName} : ${gameBoard[1].score} `;
     }
     
     const createPlayer = function({playerName, id, score = 0}) {        
