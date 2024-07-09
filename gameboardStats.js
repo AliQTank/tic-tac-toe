842ec8b8782283d82678317339792e0948990318
alber <aicarq@hotmail.com>
2024-07-09 11:38:39 -0600

    new div added for gamescore board, new js for same, added var colors to css

---
+++ gameboardStats.js
@@ -0,0 +1,1 @@
+const VisualStatsGameboard = document.getElementById("gameboardStats");
--- gridmaker.js
+++ gridmaker.js
@@ -3,6 +3,38 @@ const gridBox = gridContainer.children;
 // const arrayFromContainer = Array.from(gridContainer);
 const arrayFromTheGrids = Array.from(gridBox);
 
+/* // FUNCTION UNVIVERSALCONTROLER NOT WORKING
+const universalControler = (e) => {
+    const item = e.target;
+    const boxSelected = () => {
+        if (item.classList == "selected") {} else if (item.classList[0] == "grid-item") {
+            item.classList = "selected";
+            // console.log(e.target);
+        }
+    }
+    const getElementIndex = () => {
+        let index = Array.prototype.slice.call(item.parentElement.children).indexOf(item);
+        console.log(index); 
+    }
+    const returnElementIndex = () => {
+        let index = Array.prototype.slice.call(item.parentElement.children).indexOf(item);
+        return index;
+
+    }
+    const executerFunction = () => {
+        let index = Array.prototype.slice.call(item.parentElement.children).indexOf(item);
+        console.log(gameBoardObject.gameStatus.gameInProgress().chooseBoardLocation(index)); 
+    }
+    const functionToPrintCell = () => {
+        let index = Array.prototype.slice.call(item.parentElement.children).indexOf(item);
+        arrayCell[index].innerText = gameBoardObject.retCurrentPlayer();
+
+    }
+    return {boxSelected, getElementIndex, returnElementIndex, executerFunction, functionToPrintCell}
+}
+*/
+
+
 const boxSelected = function(e) {
     const item = e.target;
     if (item.classList == "selected") {} else if (item.classList[0] == "grid-item") {
@@ -32,7 +64,7 @@ const executerFunction = (e) => {
 const functionToPrintCell = (e) => {
     const item = e.target;
     var index = Array.prototype.slice.call(item.parentElement.children).indexOf(item);
-    arrayCell[index].innerText = gameBoardObject.retCurrentPlayer()
+    arrayCell[index].innerText = gameBoardObject.retCurrentPlayer();
 }
 
 const cleanVisualgameBoard = () => {
