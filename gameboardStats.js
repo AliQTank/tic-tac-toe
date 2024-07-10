f6a4b2ba8d856c80f0c5afe960ea728e9d7fe476
alber <aicarq@hotmail.com>
2024-07-10 16:10:16 -0600

    created player divs and vars

--- gameboardStats.js
+++ gameboardStats.js
@@ -1,1 +1,4 @@
 const VisualStatsGameboard = document.getElementById("gameboardStats");
+const player1 = document.getElementById("player1");
+const player2 = document.getElementById("player2");
+const createParagraph = document.createElement("p");
