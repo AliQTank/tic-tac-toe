70d8a79a911087cb95fa81e53f8d630ead017a54
alber <aicarq@hotmail.com>
2024-07-11 14:59:40 -0600

    creating gameboard section for controls

--- gameboardStats.js
+++ gameboardStats.js
@@ -1,4 +1,9 @@
 const VisualStatsGameboard = document.getElementById("gameboardStats");
 const player1 = document.getElementById("player1");
 const player2 = document.getElementById("player2");
-const createParagraph = document.createElement("p");
+const para1 = document.createElement("p");
+const para2 = document.createElement("p");
+para1.innerText ="first step";
+para2.innerText = "second step";
+player1.appendChild(para1);
+player2.appendChild(para2);
