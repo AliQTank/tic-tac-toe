2aef1973afb146910049ba6aa0eb59942b5b2ca6
alber <aicarq@hotmail.com>
2024-07-16 12:25:48 -0600

    innertext chgned

--- gameboardStats.js
+++ gameboardStats.js
@@ -9,7 +9,7 @@ const scorep2 = document.createElement("p");
 const buttonToStartNewRound = document.createElement("button");
 const howManyGamesPlayed = document.createElement("p");
 const currentPlayerIndicator = document.createElement("p"); //NOT USED YET
-buttonToStartNewRound.innerText = "not started";
+buttonToStartNewRound.innerText = "start".toUpperCase();
 buttonToStartNewRound.addEventListener("click", cleanAndRemove);
 buttonToStartNewRound.addEventListener("click", addStarter);
 howManyGamesPlayed.innerText = `games played so far: ${gameBoardObject.gamesCounter()}`;
@@ -17,7 +17,7 @@ h2p1.innerText = gameBoardObject.player1Name();
 h2p2.innerText = gameBoardObject.player2Name();
 scorep1.innerText = gameBoardObject.player1Score();
 scorep2.innerText = gameBoardObject.player2Score();
-currentPlayerIndicator.innerText = gameBoardObject.retCurrentPlayer()
+currentPlayerIndicator.innerText = "game not started"
 player1.appendChild(h2p1);
 player1.appendChild(scorep1);
 player2.appendChild(h2p2);
